62004e5a17bb7122988f44e68f177274517cd943
ratnawahyuni12 <ratna120307@gmail.com>
2026-03-15 14:20:22 +0700

    nomor 2 — pembuktian (while)

--- minggu5/Faktorial.java
+++ minggu5/Faktorial.java
@@ -10,8 +10,10 @@ public class Faktorial {
     // Brute Force : menggunakan perulangan
     int faktorialBF(int n) {
         int fakto = 1;
-        for (int i = 1; i <= n; i++) {
+        int i = 1;
+        while (i <= n) {
             fakto = fakto * i;
+            i++;
         }
         return fakto;
     }
