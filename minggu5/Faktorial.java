ab1e392d3cf0f01869bc3bf8583e06a16a726df0
ratnawahyuni12 <ratna120307@gmail.com>
2026-03-15 13:39:45 +0700

    Percobaan 5.2 — Menghitung Nilai Faktorial dengan Algoritma Brute Force dan Divide and Conquer (Faktorial.java)

---
+++ minggu5/Faktorial.java
@@ -0,0 +1,28 @@
+package minggu5;
+
+public class Faktorial {
+    int n;
+
+    public Faktorial(int n) {
+        this.n = n;
+    }
+
+    // Brute Force : menggunakan perulangan
+    int faktorialBF(int n) {
+        int fakto = 1;
+        for (int i = 1; i <= n; i++) {
+            fakto = fakto * i;
+        }
+        return fakto;
+    }
+
+    // Divide Conquer : menggunakan rekursi
+    int faktorialDC(int n) {
+        if(n == 1) {
+            return 1;
+        } else {
+            int fakto = n * faktorialDC(n - 1);
+            return fakto;
+        }
+    }
+}
